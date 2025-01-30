3af86577859fcdbfe7ab1815c2b593bb1cc01dea
Cyprian Wacław <cyprianwaclaw@gmail.com>
2025-01-30 16:10:38 +0100

    🚀  Change render for clinet side

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  ssr: false,
   modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@pinia/nuxt', '@nuxt/image'],
   css: ['@/assets/style/tailwind.css', '@/assets/style/style.scss']
 })
